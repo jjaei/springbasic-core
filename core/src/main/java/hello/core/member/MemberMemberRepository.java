501dccf16a664cdc01f3cb94e7e50b5e068df5e2
jjaei <ieajnowk@gmail.com>
2023-05-31 08:56:33 +0900

    회원 서비스 구현체

--- core/src/main/java/hello/core/member/MemberMemberRepository.java
+++ core/src/main/java/hello/core/member/MemberMemberRepository.java
@@ -4,14 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MemberMemberRepository implements MemberRepository{
-
     private static Map<Long, Member> store = new HashMap<>();
 
     @Override
     public void save(Member member) {
         store.put(member.getId(), member);
     }
-
 
     @Override
     public Member findById(Long memberId) {
